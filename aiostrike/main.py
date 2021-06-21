a54c0ebb6d93858a1e9f3e72d201317f17b208a0
Ali™ <76595455+Ali-TM-original@users.noreply.github.com>
2021-06-21 12:02:38 +0500

    Added 3rd endpoint

--- aiostrike/main.py
+++ aiostrike/main.py
@@ -1,6 +1,9 @@
+import json
+
 import aiohttp
 
 from .errors import ExceptionCheck
+from .errors.exceptions import RateLimitted
 
 
 class Aiostrike:
@@ -8,12 +11,13 @@ class Aiostrike:
         self.base_url = "http://csgobackpack.net/api"
         self.get_price_url = "http://csgobackpack.net/api/GetItemPrice"
         self.worth_url = "http://csgobackpack.net/api/GetInventoryValue/"
+        self.get_item_list_url = "http://csgobackpack.net/api/GetItemsList/"
         header = {
         }
         self.session = aiohttp.ClientSession(headers=header, timeout=aiohttp.ClientTimeout(total=60))
 
     async def GetItemPrice(self, item_id: str, time: int = 7, extend: int = 0, full: int = 0,
-                           currency: str = "USD", icon: int = 1, key: int = None):
+                           currency: str = "USD", icon: int = 1, key: str = None):
         parameters = {
             "id": item_id,
             "time": time,
@@ -35,9 +39,17 @@ class Aiostrike:
         async with self.session.get(self.get_price_url, params=parameters) as resp:
             ExceptionCheck(resp.status)
             data = await resp.read()
-        return data.decode()
+        x = json.loads(data.decode())
+        try:
+            x = x['success']
+        except KeyError:
+            return data.decode()
+        if x != "exceeded maximum number of requests, try again in next hour":
+            return data.decode()
+        else:
+            raise RateLimitted()
 
-    async def GetInventoryValue(self, user_id: int, ref: int = 0, currency: int = "USD", key: int = None):
+    async def GetInventoryValue(self, user_id: int, ref: int = 0, currency: int = "USD", key: str = None):
         parameters = {
             "id": user_id,
             "ref": ref,
@@ -54,7 +66,37 @@ class Aiostrike:
         async with self.session.get(self.worth_url, params=parameters) as resp:
             ExceptionCheck(resp.status)
             data = await resp.read()
-        return data.decode()
+        x = json.loads(data.decode())
+        try:
+            x = x['success']
+        except KeyError:
+            return data.decode()
+        if x != "exceeded maximum number of requests, try again in next hour":
+            return data.decode()
+        else:
+            raise RateLimitted()
+
+    async def GetItemList(self, key: str, details: int = None):
+        parameters = {
+            "key": key,
+        }
+        if details:
+            parameters = {
+                "key": key,
+                "details": details,
+            }
+        async with self.session.get(self.get_item_list_url, params=parameters) as resp:
+            ExceptionCheck(resp.status)
+            data = await resp.read()
+        x = json.loads(data.decode())
+        try:
+            x = x['success']
+        except KeyError:
+            return data.decode()
+        if x != "exceeded maximum number of requests, try again in next hour":
+            return data.decode()
+        else:
+            raise RateLimitted()
 
     async def close(self):
         return await self.session.close()
