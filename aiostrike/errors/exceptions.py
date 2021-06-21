b2815031fbd2815185c5d5455aa00cb4af11468d
Ali™ <76595455+Ali-TM-original@users.noreply.github.com>
2021-06-21 12:03:17 +0500

    Added RateLimitted Exception

--- aiostrike/errors/exceptions.py
+++ aiostrike/errors/exceptions.py
@@ -1,4 +1,3 @@
-
 class InternalServerError(Exception):
     def __init__(self, message: str = 'oopsie looks like something on our server broke again'):
         super(InternalServerError, self).__init__(message)
@@ -19,3 +18,8 @@ class BadGateway(Exception):
 class PageNotFound(Exception):
     def __init__(self, message: str = "Page Not Found 404"):
         super(PageNotFound, self).__init__(message)
+
+
+class RateLimitted(Exception):
+    def __init__(self, message: str = "Please Try Again after sometime you are being ratelimitted"):
+        super(RateLimitted, self).__init__(message)
